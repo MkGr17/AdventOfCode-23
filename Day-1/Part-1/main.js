61db083f861417dd8bc937ad29720522e307ec9d
mgroe <MkGr17@hotmail.de>
2023-12-11 11:24:17 +0100

    intial commit, day 1 task 1 done

---
+++ Day-1/Part-1/main.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const allContents = fs.readFileSync('input.txt', 'utf-8');
+let result = 0;
+
+allContents.split(/\r?\n/).forEach((line) => {
+
+    const value = getValue(line);
+    result += value;
+
+});
+
+console.log(result);
+function getValue (line) {
+
+    const first = (line.match(/\d/) || [])[0];
+    const last = (line.match(/\d(?=\D*$)/) || [])[0];
+    const result = first + '' + last;
+
+    return parseInt(result);
+
+}
